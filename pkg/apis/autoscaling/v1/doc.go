5e012ff078d6e1c0c723f8bdc4ea90b88b3e8b8a
Abu Kashem <akashem@redhat.com>
2019-12-15 00:18:15 -0500

    Add go type(s) that represents the operator API

---
+++ pkg/apis/autoscaling/v1/doc.go
@@ -0,0 +1,6 @@
+// +k8s:deepcopy-gen=package
+// +k8s:conversion-gen=github.com/openshift/cluster-resource-override-admission-operator/pkg/apis/autoscaling
+// +groupName=autoscaling.openshift.io
+
+// Package v1 contains resources types for version v1 of the operators.autoscaling.openshift.io API group.
+package v1
---
+++ pkg/apis/autoscaling/v1/override.go
@@ -0,0 +1,52 @@
+package v1
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (in *ClusterResourceOverride) IsTimeToRotateCert() bool {
+	if in.Status.CertsRotateAt.IsZero() {
+		return false
+	}
+
+	now := metav1.Now()
+	if !in.Status.CertsRotateAt.Before(&now) {
+		return false
+	}
+
+	return true
+}
+
+func (in *PodResourceOverrideSpec) String() string {
+	return fmt.Sprintf("MemoryRequestToLimitPercent=%d, CPURequestToLimitPercent=%d LimitCPUToMemoryPercent=%d",
+		in.MemoryRequestToLimitPercent, in.CPURequestToLimitPercent, in.LimitCPUToMemoryPercent)
+}
+
+func (in *PodResourceOverrideSpec) Validate() error {
+	if in.MemoryRequestToLimitPercent < 0 || in.MemoryRequestToLimitPercent > 100 {
+		return errors.New("invalid value for MemoryRequestToLimitPercent, must be [0...100]")
+	}
+
+	if in.CPURequestToLimitPercent < 0 || in.CPURequestToLimitPercent > 100 {
+		return errors.New("invalid value for CPURequestToLimitPercent, must be [0...100]")
+	}
+
+	if in.LimitCPUToMemoryPercent < 0 {
+		return errors.New("invalid value for LimitCPUToMemoryPercent, must be a positive value")
+	}
+
+	return nil
+}
+
+func (in *PodResourceOverrideSpec) Hash() string {
+	value := fmt.Sprintf("%s", in)
+
+	writer := sha256.New()
+	writer.Write([]byte(value))
+	return hex.EncodeToString(writer.Sum(nil))
+}
