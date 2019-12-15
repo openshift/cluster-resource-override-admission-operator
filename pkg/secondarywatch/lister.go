94eb6f3017ec7c4e5caef90f906519e726dfc6ea
Abu Kashem <akashem@redhat.com>
2019-12-15 00:27:46 -0500

    Add logic to watch secondary resources
    The operator needs to watch a set of secondary resources that it
    manages. When these secondary resource(s) changes, the owner of
    must be enqueued so that the operator can take appropriate actions.
    
    - Create informer for these secondary resource(s)
    - Create lister associated with these secondary resources so that the
      the reconciliation logic can use the lister to query these secondary
      resource(s) from cache.
    - Add a new 'cache.ResourceEventHandler' that will enqueue the owner
      object when a secondary resource changes - it gets updated,
      deleted or created.

---
+++ pkg/secondarywatch/lister.go
@@ -0,0 +1,43 @@
+package secondarywatch
+
+import (
+	admissionregistrationv1beta1 "k8s.io/client-go/listers/admissionregistration/v1beta1"
+	listersappsv1 "k8s.io/client-go/listers/apps/v1"
+	listerscorev1 "k8s.io/client-go/listers/core/v1"
+)
+
+// Lister is a set of Lister(s) for secondary resource(s)
+type Lister struct {
+	deployment     listersappsv1.DeploymentLister
+	daemonset      listersappsv1.DaemonSetLister
+	pod            listerscorev1.PodLister
+	configmap      listerscorev1.ConfigMapLister
+	service        listerscorev1.ServiceLister
+	secret         listerscorev1.SecretLister
+	serviceaccount listerscorev1.ServiceAccountLister
+	webhook        admissionregistrationv1beta1.MutatingWebhookConfigurationLister
+}
+
+func (l *Lister) CoreV1ConfigMapLister() listerscorev1.ConfigMapLister {
+	return l.configmap
+}
+
+func (l *Lister) CoreV1SecretLister() listerscorev1.SecretLister {
+	return l.secret
+}
+
+func (l *Lister) CoreV1ServiceLister() listerscorev1.ServiceLister {
+	return l.service
+}
+
+func (l *Lister) AppsV1DeploymentLister() listersappsv1.DeploymentLister {
+	return l.deployment
+}
+
+func (l *Lister) AppsV1DaemonSetLister() listersappsv1.DaemonSetLister {
+	return l.daemonset
+}
+
+func (l *Lister) AdmissionRegistrationV1beta1MutatingWebhookConfigurationLister() admissionregistrationv1beta1.MutatingWebhookConfigurationLister {
+	return l.webhook
+}
---
+++ pkg/secondarywatch/start.go
@@ -0,0 +1,87 @@
+package secondarywatch
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+
+	"github.com/openshift/cluster-resource-override-admission-operator/pkg/runtime"
+	"k8s.io/client-go/informers"
+	"time"
+)
+
+type Options struct {
+	Client       *runtime.Client
+	ResyncPeriod time.Duration
+	Namespace    string
+}
+
+// StarterFunc refers to a function that can be called to start watch on secondary resources.
+type StarterFunc func(enqueuer runtime.Enqueuer, shutdown context.Context) error
+
+func (s StarterFunc) Start(enqueuer runtime.Enqueuer, shutdown context.Context) error {
+	return s(enqueuer, shutdown)
+}
+
+// New sets up watch on secondary resources.
+// The function returns lister(s) that can be used to query secondary resources
+// and a StarterFunc that can be called to start the watch.
+func New(options *Options) (lister *Lister, startFunc StarterFunc) {
+	option := informers.WithNamespace(options.Namespace)
+	factory := informers.NewSharedInformerFactoryWithOptions(options.Client.Kubernetes, options.ResyncPeriod, option)
+
+	deployment := factory.Apps().V1().Deployments()
+	daemonset := factory.Apps().V1().DaemonSets()
+	pod := factory.Core().V1().Pods()
+	configmap := factory.Core().V1().ConfigMaps()
+	service := factory.Core().V1().Services()
+	secret := factory.Core().V1().Secrets()
+	serviceaccount := factory.Core().V1().ServiceAccounts()
+	webhook := factory.Admissionregistration().V1beta1().MutatingWebhookConfigurations()
+
+	startFunc = func(enqueuer runtime.Enqueuer, shutdown context.Context) error {
+		handler := newResourceEventHandler(enqueuer)
+
+		deployment.Informer().AddEventHandler(handler)
+		daemonset.Informer().AddEventHandler(handler)
+		pod.Informer().AddEventHandler(handler)
+		configmap.Informer().AddEventHandler(handler)
+		service.Informer().AddEventHandler(handler)
+		secret.Informer().AddEventHandler(handler)
+		serviceaccount.Informer().AddEventHandler(handler)
+		webhook.Informer().AddEventHandler(handler)
+
+		factory.Start(shutdown.Done())
+		status := factory.WaitForCacheSync(shutdown.Done())
+		if names := check(status); len(names) > 0 {
+			return fmt.Errorf("WaitForCacheSync did not successfully complete resources=%s", names)
+		}
+
+		return nil
+	}
+
+	lister = &Lister{
+		deployment:     deployment.Lister(),
+		daemonset:      daemonset.Lister(),
+		pod:            pod.Lister(),
+		configmap:      configmap.Lister(),
+		service:        service.Lister(),
+		secret:         secret.Lister(),
+		serviceaccount: serviceaccount.Lister(),
+		webhook:        webhook.Lister(),
+	}
+
+	return
+}
+
+func check(status map[reflect.Type]bool) []string {
+	names := make([]string, 0)
+
+	for objType, synced := range status {
+		if !synced {
+			names = append(names, objType.Name())
+		}
+	}
+
+	return names
+}
