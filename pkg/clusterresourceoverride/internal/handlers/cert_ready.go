7a8ac91cd48a1a3868f1603487283fb05d84ac53
Abu Kashem <akashem@redhat.com>
2020-01-06 14:19:38 -0500

    Fix issues related to failed test(s)
    - Fix cert rotation issue.
    - Fix logic that reports true when DaemonSet is ready. It currently
      reports ready prematurely.

--- pkg/clusterresourceoverride/internal/handlers/cert_ready.go
+++ pkg/clusterresourceoverride/internal/handlers/cert_ready.go
@@ -27,38 +27,37 @@ func (c *certReadyHandler) Handle(context *ReconcileRequestContext, original *au
 	current = original
 	resources := original.Status.Resources
 
+	if context.GetBundle() == nil {
+		secret, err := c.lister.CoreV1SecretLister().Secrets(context.WebhookNamespace()).Get(resources.ServiceCertSecretRef.Name)
+		if err != nil {
+			handleErr = condition.NewInstallReadinessError(autoscalingv1.CertNotAvailable, err)
+			return
+		}
+
+		configmap, err := c.lister.CoreV1ConfigMapLister().ConfigMaps(context.WebhookNamespace()).Get(resources.ServiceCAConfigMapRef.Name)
+		if err != nil {
+			handleErr = condition.NewInstallReadinessError(autoscalingv1.CertNotAvailable, err)
+			return
+		}
+
+		servingCertCA := []byte(configmap.Data["service-ca.crt"])
+		bundle := &cert.Bundle{
+			Serving: cert.Serving{
+				ServiceKey:  secret.Data["tls.key"],
+				ServiceCert: secret.Data["tls.crt"],
+			},
+			ServingCertCA: servingCertCA,
+		}
+
+		if err := bundle.Validate(); err != nil {
+			handleErr = condition.NewInstallReadinessError(autoscalingv1.CertNotAvailable, fmt.Errorf("certs not populated - %s", err.Error()))
+			return
+		}
+
+		context.SetBundle(bundle)
+	}
+
 	bundle := context.GetBundle()
-	if bundle != nil {
-		return
-	}
-
-	secret, err := c.lister.CoreV1SecretLister().Secrets(context.WebhookNamespace()).Get(resources.ServiceCertSecretRef.Name)
-	if err != nil {
-		handleErr = condition.NewInstallReadinessError(autoscalingv1.CertNotAvailable, err)
-		return
-	}
-
-	configmap, err := c.lister.CoreV1ConfigMapLister().ConfigMaps(context.WebhookNamespace()).Get(resources.ServiceCAConfigMapRef.Name)
-	if err != nil {
-		handleErr = condition.NewInstallReadinessError(autoscalingv1.CertNotAvailable, err)
-		return
-	}
-
-	servingCertCA := []byte(configmap.Data["service-ca.crt"])
-	bundle = &cert.Bundle{
-		Serving: cert.Serving{
-			ServiceKey:  secret.Data["tls.key"],
-			ServiceCert: secret.Data["tls.crt"],
-		},
-		ServingCertCA: servingCertCA,
-	}
-
-	if err := bundle.Validate(); err != nil {
-		handleErr = condition.NewInstallReadinessError(autoscalingv1.CertNotAvailable, fmt.Errorf("certs not populated - %s", err.Error()))
-		return
-	}
-
-	context.SetBundle(bundle)
 	current.Status.Hash.ServingCert = bundle.Hash()
 
 	klog.V(2).Infof("key=%s cert check passed", original.Name)
