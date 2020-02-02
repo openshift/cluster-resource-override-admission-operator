883dceb560883db5c259fb7304fa683cafe3aaaf
Abu Kashem <akashem@redhat.com>
2020-02-02 18:51:19 -0500

    Fix wait-for-deployment.sh to make e2e with olm work
    wait-for-deployment.sh should fail if kubectl/oc binary is not
    present instead of giving false message of retries.

--- hack/wait-for-deployment.sh
+++ hack/wait-for-deployment.sh
@@ -5,13 +5,16 @@ set -o nounset
 set -o pipefail
 
 util::await_operator_deployment_create() {
-    local namespace="$1"
-    local name="$2"
-    local retries="${3:-50}"
+    local kubectl="$1"
+    local namespace="$2"
+    local name="$3"
+    local retries="${4:-50}"
     local output
 
+    command -v ${kubectl} >/dev/null 2>&1 || { echo >&2 "${kubectl} not found, aborting."; exit 1; }
+
     until [[ "${retries}" -le "0" ]]; do
-        output=$(kubectl get deployment -n "${namespace}" "${name}" -o jsonpath='{.metadata.name}' 2>/dev/null || echo "waiting for olm to deploy the operator")
+        output=$(${kubectl} get deployment -n "${namespace}" "${name}" -o jsonpath='{.metadata.name}' 2>/dev/null || echo "waiting for olm to deploy the operator")
 
         if [ "${output}" = "${name}" ] ; then
             echo "${namespace}/${name} has been created" >&2
@@ -28,9 +31,10 @@ util::await_operator_deployment_create() {
     return 1
 }
 
-NAMESPACE=$1
-DEPLOYMENT_NAME=$2
-WAIT_TIME=$3
+KUBECTL=$1
+NAMESPACE=$2
+DEPLOYMENT_NAME=$3
+WAIT_TIME=$4
 
-exitcode=$(util::await_operator_deployment_create "${NAMESPACE}" "${DEPLOYMENT_NAME}" "${WAIT_TIME}")
+exitcode=$(util::await_operator_deployment_create "${KUBECTL}" "${NAMESPACE}" "${DEPLOYMENT_NAME}" "${WAIT_TIME}")
 exit $exitcode
