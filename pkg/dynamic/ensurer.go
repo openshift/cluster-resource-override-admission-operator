9569a44d049aef176ed1f669f9cfb5c2f7e826d0
John Kyros <jkyros@redhat.com>
2024-10-02 10:09:44 -0500

    re-inject service account secrets during ensure
    So OpenShift did a clever thing, and has openshift-controller-manager
    creating service account tokens for service accounts that don't have
    them. It turns out, that if our operand pods fail to start, the operator
    will continue ensuring the service account, and because of the "generic
    nature" of the dynamic ensure client, it strips the secret fields every
    time it ensures, resulting in openshift-controller-manager thinking the
    service account has no tokens and making a new one.
    
    That behavior resulted in the cluster filling up with secrets if the CRO
    operands failed to start.
    
    To fix said behavior, this just adds a special case for service accounts
    to make sure that any secrets that have been assigned to the service
    account are preserved across "ensures" so the
    openshift-controller-manager doesn't think it needs to make more
    secrets.
    
    Signed-off-by: John Kyros <jkyros@redhat.com>

--- pkg/dynamic/ensurer.go
+++ pkg/dynamic/ensurer.go
@@ -64,9 +64,26 @@ func (c *client) Ensure(resource string, object runtime.Object) (current *unstru
 	modified.SetResourceVersion(original.GetResourceVersion())
 	modified.SetUID(original.GetUID())
 
+	// jkyros: I hate to lodge a special case in this generic ensure function, but OpenShift has special
+	// secret behavior that auto-populates tokens for service accounts, and if we strip them out, the openshift-controller-manager
+	// will keep recreating a ton of secrets in response, see https://issues.redhat.com/browse/OCPBUGS-15462
+	if kind == "ServiceAccount" {
+		// If we have secrets in the original service account, stuff them into the modified so they don't get patched out
+		originalSecrets, secretsFound, err := unstructuredv1.NestedSlice(original.Object, "secrets")
+		if secretsFound && err == nil {
+			unstructuredv1.SetNestedSlice(modified.Object, originalSecrets, "secrets")
+		}
+		// If we have image pull secrets in the original service account, stuff them into the modified so they don't get patched out
+		originalImagePullSecrets, imagePullSeecretsFound, err := unstructuredv1.NestedSlice(original.Object, "imagePullSecrets")
+		if imagePullSeecretsFound && err == nil {
+			unstructuredv1.SetNestedSlice(modified.Object, originalImagePullSecrets, "imagePullSecrets")
+		}
+	}
+
 	bytes, patchErr := PatchWithUnstructured(original, modified, object)
 	if patchErr != nil {
 		err = fmt.Errorf("failed to generate patch %s - %s", kind, patchErr.Error())
+		// TODO(jkyros): If this fails we patch anyway, but it's been like this for so long, I hate to change it
 	}
 
 	current, err = client.Patch(context.TODO(), modified.GetName(), types.StrategicMergePatchType, bytes, metav1.PatchOptions{})
