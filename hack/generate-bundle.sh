036c2681a31d02ce2e441f1cffcfad36adefa6d3
Max Cao <macao@redhat.com>
2025-04-24 10:44:24 -0700

    Generate a bundle for CI to consume
    Renames build-bundle.sh -> generate-bundle.sh and removes the removal of the bundle to commit it instead.
    We need this because it makes it easier for CI to run operator-sdk upgrade tests by allowing it to consume an existing bundle
    and promote the bundle image through CI.
    
    Signed-off-by: Max Cao <macao@redhat.com>

--- hack/generate-bundle.sh
+++ hack/generate-bundle.sh
@@ -2,39 +2,38 @@
 
 set -euo pipefail
 
+# TODO(maxcao13): This is hack script to generate an OLM bundle
+# We should remove it if we migrate to operator-sdk project and use `make bundle` instead
+
 # Required: OPERATOR_IMG, OPERAND_IMG, BUNDLE_IMG environment variables
+# Optional: SKIP_BUILD
 #
 # This script does a number of steps.
-# 1. Builds the binary, builds the operator image, and pushes it to your specified repo.
+# 1. Builds the binary, builds the operator image (if enabled), and pushes it to your specified repo.
 # 2. Generates an OLM bundle with the specified operator and operand img.
-# 3. Builds and pushes a bundle image.
+# 3. Builds and pushes a bundle image, if SKIP_BUILD is not true.
 # 
 # e.g.,
 # 
 #   OPERATOR_IMG=quay.io/macao/clusterresourceoverride-operator:VERSION \
 #   OPERAND_IMG=quay.io/macao/clusterresourceoverride:VERSION \
 #   BUNDLE_IMG=quay.io/macao/cro-bundle:VERSION \
-#   ./hack/build-bundle.sh
+#   ./hack/generate-bundle.sh
 
-REQUIRED_ENV_VARS=("OPERATOR_IMG" "OPERAND_IMG" "BUNDLE_IMG")
-check_env_vars() {
-    local REQUIRED_VARS=("$@")
-    for VAR_NAME in "${REQUIRED_VARS[@]}"; do
-        if [ -z "${!VAR_NAME:-}" ]; then
-            echo "Error: $VAR_NAME environment variable is not set." ; exit 1
-        fi
-    done
-}
-
-check_env_vars "${REQUIRED_ENV_VARS[@]}"
+operator_img=${OPERATOR_IMG:-"quay.io/placeholder/operator:1.0"}
+operand_img=${OPERAND_IMG:-"quay.io/placeholder/operand:1.0"}
+bundle_img=${BUNDLE_IMG:-"quay.io/placeholder/bundle:1.0"}
+skip_build=${SKIP_BUILD:-"false"}
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 ROOT_DIR=$SCRIPT_DIR/..
 
 # Build and push the operator image
-make build
-podman build -t "$OPERATOR_IMG" -f ./images/dev/Dockerfile.dev .
-podman push "$OPERATOR_IMG"
+if [ "$skip_build" == "false" ]; then
+    make build
+    podman build -t "$operator_img" -f ./images/dev/Dockerfile.dev .
+    podman push "$operator_img"
+fi
 
 rm -rf bundle
 
@@ -44,8 +43,6 @@ awk 'FNR==1 && NR!=1 {print "---"} {print}' artifacts/deploy/* manifests/stable/
     --default-channel stable \
     --channels stable
 
-trap "rm -rf bundle" EXIT
-
 CSV_BUNDLE_PATH=$ROOT_DIR/bundle/manifests/clusterresourceoverride-operator.clusterserviceversion.yaml
 
 if ! [ -e "$CSV_BUNDLE_PATH" ]; then
@@ -53,13 +50,15 @@ if ! [ -e "$CSV_BUNDLE_PATH" ]; then
 fi
 
 # Update the CSV bundle with correct image references
-sed -e "s|CLUSTERRESOURCEOVERRIDE_OPERATOR_IMAGE|$OPERATOR_IMG|g" \
-    -e "s|CLUSTERRESOURCEOVERRIDE_OPERAND_IMAGE|$OPERAND_IMG|g" \
+sed -e "s|CLUSTERRESOURCEOVERRIDE_OPERATOR_IMAGE|$operator_img|g" \
+    -e "s|CLUSTERRESOURCEOVERRIDE_OPERAND_IMAGE|$operand_img|g" \
     "$CSV_BUNDLE_PATH" -i
 
 # Validate the bundle
 operator-sdk bundle validate ./bundle
 
 # Build and push the bundle image
-podman build -t "$BUNDLE_IMG" -f bundle.Dockerfile .
-podman push "$BUNDLE_IMG"
+if [ "$skip_build" == "false" ]; then
+    podman build -t "$bundle_img" -f bundle.Dockerfile .
+    podman push "$bundle_img"
+fi
